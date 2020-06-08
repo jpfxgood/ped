1f54d4a276f95d80a730fbc8f7350006545ea23b
jpfxgood <jpfx@jlgoodwin.com>
2020-06-08 16:38:33 -0400

    Create framework to test the main editor including curses rendering

--- 2to3out/tests/test_editor_common.py
+++ 2to3out/tests/test_editor_common.py
@@ -2,13 +2,14 @@ import editor_common
 import io
 import pprint
 import os
+import curses
+import curses.ascii
+import time
 
 def test_memline():
     m = editor_common.MemLine( "01234567890123456789" )
     assert( m.length() == 20 )
     assert( m.getContent() == "01234567890123456789" )
-
-
 
 def test_EditFile(testdir):  
     lines_to_test = ["This is the first line","This is the second line","This is the third line","This is the last line"]
@@ -102,4 +103,53 @@ def test_EditFile(testdir):
     fls = ef.getLines()
     for line in range(0,len(lines_to_test)):
         assert(fls[line].rstrip() == lines_to_test[line])
-    assert(fls[9].rstrip() == new_test_line)
+    assert(fls[9].rstrip() == new_test_line) 
+    newname = os.path.join(fd,"1_"+os.path.basename(fn))
+    ef.save(newname)
+    assert(os.path.exists(newname))
+    ef.close()
+    ef.load()
+    assert(ef.getFilename() == newname)
+    fls = ef.getLines()
+    for line in range(0,len(lines_to_test)):
+        assert(fls[line].rstrip() == lines_to_test[line])
+    assert(fls[9].rstrip() == new_test_line) 
+    assert(ef.get_tab_stop(4) == 8)
+    assert(ef.get_tab_stop(10) == 16 )
+    assert(ef.get_tab_stop(10,True) == 8)  
+    tabby_string = "01234\t56789012\t3456789"
+    expanded_string = "01234   56789012        3456789"
+    assert(ef.expand_tabs(tabby_string) == expanded_string)
+    
+def test_Editor(testdir,capsys):
+    with capsys.disabled():
+        lines_to_test = ["This is the first line","This is the second line","This is the third line","This is the last line"]
+        testfile = testdir.makefile(".txt",lines_to_test[0],lines_to_test[1],lines_to_test[2],lines_to_test[3])
+        fn = str(testfile)
+        
+        def read_str( win, y, x, width ):
+            out_str = ''
+            for ix in range(x,x+width):
+                rc = win.inch(y,ix)
+                out_str += chr(rc & 0x00FF)
+            return out_str
+
+        def match_attr( win, y, x, height, width, attr ):
+            for iy in range(y,y+height):
+                for ix in range(x,x+width):
+                    rc = win.inch(iy,ix)
+                    if not (attr & rc):
+                        return(False)
+            return(True)
+                        
+            
+        def main(stdscr):
+            ed = editor_common.Editor(stdscr,None,fn)
+            ed.setWin(stdscr.subwin(ed.max_y,ed.max_x,0,0))
+            ed.main(False)
+            for ix in range(0,len(lines_to_test)):
+                assert(read_str(ed.scr,ix+1,0,len(lines_to_test[ix])) == lines_to_test[ix])
+            assert(match_attr(ed.scr,0,0,1,ed.max_x,curses.A_REVERSE))
+            time.sleep(30)
+    
+        curses.wrapper(main)
